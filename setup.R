a99cc76f38aa221ad4e52c9f94e2e583093c4fb9
Tom Palmer <remlapmot@hotmail.com>
2026-02-27 11:22:27 +0000

    Update setup.R [skip ci]

--- setup.R
+++ setup.R
@@ -12,3 +12,10 @@ library(orgmetricsDashboard)
 
 # Create a workflow file in the default location
 use_github_action_orgmetrics()
+
+# options(repos = c(
+#   ropenscireviewtools = "https://ropensci-review-tools.r-universe.dev",
+#   ropensci = "https://ropensci.r-universe.dev",
+#   CRAN = "https://cloud.r-project.org"
+# ))
+# install.packages("orgmetrics")
