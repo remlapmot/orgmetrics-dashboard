67cce09bfaa6b484802fb8e6388652e512708463
Tom Palmer <remlapmot@hotmail.com>
2026-02-27 11:06:02 +0000

    Create setup.R

---
+++ setup.R
@@ -0,0 +1,14 @@
+# reprex::reprex({
+# options (repos = c(
+#   ropenscireviewtools = "https://ropensci-review-tools.r-universe.dev",
+#   CRAN = "https://cloud.r-project.org"
+# ))
+# install.packages("orgmetricsDashboard")
+# })
+
+# remotes::install_github ("ropensci-review-tools/orgmetricsDashboard")
+
+library(orgmetricsDashboard)
+
+# Create a workflow file in the default location
+use_github_action_orgmetrics()
